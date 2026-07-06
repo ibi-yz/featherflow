44a9787449a4d0b8a20b8896f772842ccc194c41
ibi-yz <mibrahim.yz2009@gmail.com>
2026-07-06 16:15:58 +0500

    feat: add Bird model with name, species, age, gender

---
+++ lib/models/bird.dart
@@ -0,0 +1,15 @@
+//custom class for digital aviary
+
+class Bird {
+  final String name;
+  final String species;
+  final String age;
+  final String gender;
+
+  const Bird({
+    required this.name,
+    required this.species,
+    required this.age,
+    required this.gender,
+  });
+}
