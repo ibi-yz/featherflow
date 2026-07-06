15e02d3420a8d3ff28b0b18c83cfbcb18f8be02a
ibi-yz <mibrahim.yz2009@gmail.com>
2026-07-06 20:26:06 +0500

    feat: add FAB navigation to AddBirdScreen

--- lib/screens/aviary_screen.dart
+++ lib/screens/aviary_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:featherflow/models/bird.dart';
+import 'package:featherflow/screens/add_bird_screen.dart';
 
 class AviaryScreen extends StatefulWidget {
   const AviaryScreen({super.key});
@@ -8,39 +9,49 @@ class AviaryScreen extends StatefulWidget {
   State<AviaryScreen> createState() => _AviaryScreenState();
 }
 
-
-
 class _AviaryScreenState extends State<AviaryScreen> {
   final List<Bird> birds = [
     Bird(name: 'Banana', species: 'Budgie', age: '3 months', gender: 'male'),
     Bird(name: 'Apple', species: 'Cockatiel', age: '1 year', gender: 'Female'),
-    Bird(name: 'Einstein', species: "Congo African Grey", age: '7 year', gender: 'male'),
+    Bird(
+      name: 'Einstein',
+      species: "Congo African Grey",
+      age: '7 year',
+      gender: 'male',
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Digital Aviary'),
-        centerTitle: true,
-      ),
+      appBar: AppBar(title: Text('Digital Aviary'), centerTitle: true),
       body: ListView.builder(
         padding: EdgeInsets.all(8),
         itemCount: birds.length,
-        itemBuilder: (context, index){
-          final Bird = birds[index];
+        itemBuilder: (context, index) {
+          final bird = birds[index];
           return Card(
             child: ListTile(
-              leading: CircleAvatar(child: Icon(Icons.flutter_dash),),
-              title: Text(Bird.name),
-              subtitle: Text('${Bird.species} | ${Bird.age} | ${Bird.gender}'),
-              trailing: Icon(Icons.arrow_forward_ios, size: 22,),
+              leading: CircleAvatar(child: Icon(Icons.flutter_dash)),
+              title: Text(bird.name),
+              subtitle: Text('${bird.species} | ${bird.age} | ${bird.gender}'),
+              trailing: Icon(Icons.arrow_forward_ios, size: 22),
             ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {Text('button working ');},
+        onPressed: () async {
+          final newBird = await Navigator.push<Bird>(
+            context,
+            MaterialPageRoute(builder: (context) => const AddBirdScreen()),
+          );
+          if (newBird != null) {
+            setState(() {
+              birds.add(newBird);
+            });
+          }
+        },
         child: Icon(Icons.add),
       ),
     );
