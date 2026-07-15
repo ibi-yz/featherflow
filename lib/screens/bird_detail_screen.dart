5d205f410c0705e462725cd6cd867cad7a07da72
ibi-yz <mibrahim.yz2009@gmail.com>
2026-07-15 23:21:46 +0500

    feat: redesign bird details sheet to M3 tall layout

--- lib/screens/bird_detail_screen.dart
+++ lib/screens/bird_detail_screen.dart
@@ -5,16 +5,75 @@ class BirdDetailsSheet extends StatelessWidget {
   final Bird bird;
   const BirdDetailsSheet({super.key, required this.bird});
 
-  static void show(BuildContext context, Bird bird) {
-    showModalBottomSheet(
-      context: context,
-      showDragHandle: true,
-      builder: (_) => BirdDetailsSheet(bird: bird),
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme = Theme.of(context).textTheme;
+    final ColorScheme = Theme.of(context).colorScheme;
+
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24),
+              color: ColorScheme.primaryContainer,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text(bird.name, style: TextTheme.headlineMedium),
+          Text(
+            '${bird.species} | ${bird.age} | ${bird.gender}',
+            style: TextTheme.bodyMedium?.copyWith(
+              color: ColorScheme.onSurfaceVariant,
+            ),
+          ),
+          const SizedBox(height: 24),
+          Row(
+            children: [
+              Expanded(
+                child: FilledButton.icon(
+                  onPressed: () {
+                    //later add delete option
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.delete),
+                  label: Text('Delete'),
+                  style: FilledButton.styleFrom(
+                    backgroundColor: ColorScheme.error,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: FilledButton.icon(
+                  onPressed: () {
+                    //later add edit bird option
+                  },
+                  label: Text("Edit"),
+                  icon: Icon(Icons.edit),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.all(24), child: Text(bird.name));
+  static void show(BuildContext context, Bird bird) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      showDragHandle: true,
+      builder: (_) => SizedBox(
+        height: MediaQuery.of(context).size.height * 0.75,
+        child: BirdDetailsSheet(bird: bird),
+      ),
+    );
   }
 }
