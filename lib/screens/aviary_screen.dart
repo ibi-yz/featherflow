75f6326cc9285e6d97d27b9fa7bcb7974bcc77a9
ibi-yz <mibrahim.yz2009@gmail.com>
2026-08-02 23:58:33 +0500

    feat: wire image edit option in edit bird screen

--- lib/screens/aviary_screen.dart
+++ lib/screens/aviary_screen.dart
@@ -3,6 +3,7 @@ import 'package:featherflow/models/bird.dart';
 import 'package:featherflow/screens/add_bird_screen.dart';
 import 'bird_detail_screen.dart';
 import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
 class AviaryScreen extends StatefulWidget {
   const AviaryScreen({super.key});
@@ -19,6 +20,7 @@ class _AviaryScreenState extends State<AviaryScreen> {
     final ageNumberController = TextEditingController();
     String? selectedGender = bird.gender;
     String? selectedUnit;
+    String? pickedImagePath = bird.imagePath;
     final genders = ['Male', 'Female', 'Unknown'];
     final units = ['Days', "Months", "Years"];
 
@@ -29,90 +31,149 @@ class _AviaryScreenState extends State<AviaryScreen> {
     } else {
       ageNumberController.text = bird.age;
     }
+
     showDialog(
       context: context,
       builder: (context) => StatefulBuilder(
-        builder: (context, setDialogState) => AlertDialog(
-          scrollable: true,
-          title: const Text("Edit Bird", style: TextStyle(fontSize: 24)),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: nameController,
-                //keyboardType: TextInputType.name,
-                decoration: const InputDecoration(labelText: 'Name'),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: speciesController,
-                //keyboardType: TextInputType.name,
-                decoration: const InputDecoration(labelText: 'Species'),
-              ),
-              const SizedBox(height: 16),
-              Row(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: TextField(
-                      controller: ageNumberController,
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(labelText: 'Age'),
-                    ),
-                  ),
-                  const SizedBox(width: 2),
-                  Expanded(
-                    flex: 2,
-                    child: DropdownButtonFormField<String>(
-                      value: selectedUnit,
-                      decoration: const InputDecoration(labelText: 'Age'),
-                      items: units
-                          .map(
-                            (g) => DropdownMenuItem(value: g, child: Text(g)),
-                          )
-                          .toList(),
-                      onChanged: (val) => setState(() => selectedUnit = val),
-                    ),
-                  ),
-                ],
-              ),
+        builder: (context, setDialogState) {
+          Future<void> _pickImage() async {
+            final XFile? image = await ImagePicker().pickImage(
+              source: ImageSource.gallery,
+            );
+            if (image != null) {
+              setDialogState(() => pickedImagePath = image.path);
+            }
+          }
 
-              const SizedBox(height: 16),
-              DropdownButtonFormField<String>(
-                value: selectedGender,
-                decoration: const InputDecoration(labelText: 'Gender'),
-                items: genders
-                    .map((g) => DropdownMenuItem(value: g, child: Text(g)))
-                    .toList(),
-                onChanged: (val) => setState(() => selectedGender = val),
+          return AlertDialog(
+            scrollable: true,
+            title: const Text("Edit Bird", style: TextStyle(fontSize: 24)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                //here i will add edit bird image
+                GestureDetector(
+                  onTap: _pickImage,
+                  child: Container(
+                    width: double.infinity,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primaryContainer,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: pickedImagePath != null
+                        ? ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: Image.file(
+                              File(pickedImagePath!),
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                        : Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.add_a_photo,
+                                  size: 30,
+                                  color: Theme.of(
+                                    context,
+                                  ).colorScheme.onPrimaryContainer,
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  'Tap to add a photo',
+                                  style: TextStyle(
+                                    color: Theme.of(
+                                      context,
+                                    ).colorScheme.onPrimaryContainer,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: nameController,
+                  //keyboardType: TextInputType.name,
+                  decoration: const InputDecoration(labelText: 'Name'),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: speciesController,
+                  //keyboardType: TextInputType.name,
+                  decoration: const InputDecoration(labelText: 'Species'),
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: TextField(
+                        controller: ageNumberController,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(labelText: 'Age'),
+                      ),
+                    ),
+                    const SizedBox(width: 2),
+                    Expanded(
+                      flex: 2,
+                      child: DropdownButtonFormField<String>(
+                        value: selectedUnit,
+                        decoration: const InputDecoration(labelText: 'Age'),
+                        items: units
+                            .map(
+                              (g) => DropdownMenuItem(value: g, child: Text(g)),
+                            )
+                            .toList(),
+                        onChanged: (val) =>
+                            setDialogState(() => selectedUnit = val),
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 16),
+                DropdownButtonFormField<String>(
+                  value: selectedGender,
+                  decoration: const InputDecoration(labelText: 'Gender'),
+                  items: genders
+                      .map((g) => DropdownMenuItem(value: g, child: Text(g)))
+                      .toList(),
+                  onChanged: (val) =>
+                      setDialogState(() => selectedGender = val),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text("Cancel"),
+              ),
+              FilledButton(
+                onPressed: () {
+                  int index = birds.indexOf(bird);
+                  Bird updated = Bird(
+                    name: nameController.text,
+                    species: speciesController.text,
+                    age:
+                        '${ageNumberController.text.trim()} ${selectedUnit ?? ''}'
+                            .trim(),
+                    gender: selectedGender!,
+                    imagePath: pickedImagePath,
+                  );
+                  setState(() {
+                    birds[index] = updated;
+                  });
+                  Navigator.pop(context);
+                },
+                child: const Text('Save'),
               ),
             ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text("Cancel"),
-            ),
-            FilledButton(
-              onPressed: () {
-                int index = birds.indexOf(bird);
-                Bird updated = Bird(
-                  name: nameController.text,
-                  species: speciesController.text,
-                  age:
-                      '${ageNumberController.text.trim()} ${selectedUnit ?? ''}'
-                          .trim(),
-                  gender: selectedGender!,
-                );
-                setState(() {
-                  birds[index] = updated;
-                });
-                Navigator.pop(context);
-              },
-              child: const Text('Save'),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
