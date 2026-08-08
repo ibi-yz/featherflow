f77f96e28035d2ba7caeda7ac07a5807aa848c37
ibi-yz <mibrahim.yz2009@gmail.com>
2026-08-08 21:55:25 +0500

    feat: add Hive persistence to Bird model

--- lib/models/bird.dart
+++ lib/models/bird.dart
@@ -1,10 +1,20 @@
-//custom class for digital aviary
+import 'package:hive/hive.dart';
 
+//Class for bird now utilizes hive adapter
+
+part 'bird.g.dart';
+
+@HiveType(typeId: 0)
 class Bird {
+  @HiveField(0)
   final String name;
+  @HiveField(1)
   final String species;
+  @HiveField(2)
   final String age;
+  @HiveField(3)
   final String gender;
+  @HiveField(4)
   String? imagePath;
 
   Bird({
---
+++ lib/models/bird.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bird.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class BirdAdapter extends TypeAdapter<Bird> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Bird read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Bird(
+      name: fields[0] as String,
+      species: fields[1] as String,
+      age: fields[2] as String,
+      gender: fields[3] as String,
+      imagePath: fields[4] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Bird obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.species)
+      ..writeByte(2)
+      ..write(obj.age)
+      ..writeByte(3)
+      ..write(obj.gender)
+      ..writeByte(4)
+      ..write(obj.imagePath);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is BirdAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
